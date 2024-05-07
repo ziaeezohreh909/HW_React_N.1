5c433c716f9cee9f6592802adab63cea820fda23
“ziaeezohreh909” <ziaeezohreh909@gmail.com>
2024-05-07 17:35:41 +0330

    create homepage

--- src/component/Home/Home.tsx
+++ src/component/Home/Home.tsx
@@ -1,8 +1,19 @@
+import Btn from "../Button/Btn";
 
 export default function Home() {
   return (
-    <div>
-      HOme
+    <div className="flex justify-between mx-14 mt-2 mb-3">
+      <div className="flex gap-1">
+        <input type="checkbox" />
+        <p>Task 1</p>
+        <button className="bg-red-500 rounded-md  font-mono text-xs text-white px-2">
+          High
+        </button>
+      </div>
+
+      <div className="flex w-1/2 px-20">
+        <Btn />
+      </div>
     </div>
-  )
+  );
 }
