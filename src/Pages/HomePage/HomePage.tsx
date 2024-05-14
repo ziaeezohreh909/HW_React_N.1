b263d0c6b9b5a39ee7f22a5e75d3e96aab66eeef
“ziaeezohreh909” <ziaeezohreh909@gmail.com>
2024-05-14 19:28:51 +0330

    background

--- src/Pages/HomePage/HomePage.tsx
+++ src/Pages/HomePage/HomePage.tsx
@@ -2,8 +2,11 @@ import Home from "../../component/Home/Home";
 
 export default function HomePage() {
   return (
-    <div className="bg-white shadow-md w-96 rounded-xl mx-auto  mt-24 ">
-      <Home />
+    <div className="w-full h-screen flex justify-center items-center">
+      <div className="w-[500px] h-[420px] bg-gradient-to-l from-blue-700 t0-blue-400 rounded-xl rotate-6 relative shadow-lg"></div>
+      <div className="bg-white shadow-md w-[500px] h-[420px] rounded-xl mx-auto  mt-24 absolute px-20 py-14 ">
+        <Home />
+      </div>
     </div>
   );
 }
