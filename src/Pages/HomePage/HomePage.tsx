f24254003cfbaf67ae2f2a4d57dd8bd83bc02093
“ziaeezohreh909” <ziaeezohreh909@gmail.com>
2024-05-14 22:41:40 +0330

    change background

--- src/Pages/HomePage/HomePage.tsx
+++ src/Pages/HomePage/HomePage.tsx
@@ -2,9 +2,9 @@ import Home from "../../component/Home/Home";
 
 export default function HomePage() {
   return (
-    <div className="w-full h-screen flex justify-center items-center">
-      <div className="w-[500px] h-[420px] bg-gradient-to-l from-blue-700 t0-blue-400 rounded-xl rotate-6 relative shadow-lg"></div>
-      <div className="bg-white shadow-md w-[500px] h-[420px] rounded-xl mx-auto  mt-24 absolute px-20 py-14 ">
+    <div className="flex justify-center items-center my-24">
+      <div className="w-[500px] h-[420px] absolute bg-blue-800 rounded-xl rotate-6  shadow-lg"></div>
+      <div className="bg-white shadow-md w-[500px] h-[420px] rounded-xl  relative px-20 py-14 ">
         <Home />
       </div>
     </div>
